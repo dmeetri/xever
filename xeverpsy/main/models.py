de77dcd28821f2f938512e1e06135ac0972abe54
dmeetri <145245244+dmeetri@users.noreply.github.com>
2025-10-26 17:01:07 +0900

    file in database

--- xeverpsy/main/models.py
+++ xeverpsy/main/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Portfolio(models.Model):
     title = models.TextField()
     description = models.TextField()
-    video = models.FileField(upload_to='video/')
+    video = models.FileField(upload_to='video/') #file in data base
     upload_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
