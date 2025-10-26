61122051b72180b0291957e10578c87b925ba80f
dmeetri <145245244+dmeetri@users.noreply.github.com>
2025-10-26 19:41:19 +0900

    fix: DRF

--- xeverpsy/main/serializer.py
+++ xeverpsy/main/serializer.py
@@ -1,16 +1,16 @@
 from rest_framework import serializers
-from .models import *
+from .models import Portfolio, Services
 
 class PortfolioSerializer(serializers.ModelSerializer):
     video = serializers.FileField(use_url=True)
-    
+
     class Meta:
-        models = Portfolio
+        model = Portfolio
         fields = '__all__'
 
 class ServicesSerializer(serializers.ModelSerializer):
     preview = serializers.ImageField(use_url=True)
     
     class Meta:
-        models = Services
+        model = Services
         fields = '__all__'
