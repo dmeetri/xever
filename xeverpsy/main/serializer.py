b97354f5262770bdcf5f2387ac8a56c00a734a2e
dmeetri <145245244+dmeetri@users.noreply.github.com>
2025-10-26 19:35:56 +0900

    fix: api

--- xeverpsy/main/serializer.py
+++ xeverpsy/main/serializer.py
@@ -2,11 +2,15 @@ from rest_framework import serializers
 from .models import *
 
 class PortfolioSerializer(serializers.ModelSerializer):
+    video = serializers.FileField(use_url=True)
+    
     class Meta:
         models = Portfolio
         fields = '__all__'
 
 class ServicesSerializer(serializers.ModelSerializer):
+    preview = serializers.ImageField(use_url=True)
+    
     class Meta:
         models = Services
         fields = '__all__'
